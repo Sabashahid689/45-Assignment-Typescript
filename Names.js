df0b7e97bf37b488c688db3dcc9b86a06e489bfa
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-03-17 17:45:43 +0500

    Add files via upload

--- Names.js
+++ Names.js
@@ -1,6 +1,7 @@
 "use strict";
-//Question no:11;
-let personName = ['saba', 'umzehra', 'alisbah', 'sana'];
+//Question no:12;
+let names = ['saba', 'umzehra', 'alisbah', 'sana'];
+const message = 'Welcome to Typescript class:';
 for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
+    console.log(message + names[i]);
 }
--- Names.ts
+++ Names.ts
@@ -1,5 +1,6 @@
-//Question no:11;
-let personName :string[] = ['saba','umzehra','alisbah','sana'];
+//Question no:12;
+let names :string[] = ['saba','umzehra','alisbah','sana'] ;
+const message :string = 'Welcome to Typescript class:'
 for(let i=0; i<names.length; i++){
-    console.log(names[i]);
+    console.log(message + names[i]) ;
 }
