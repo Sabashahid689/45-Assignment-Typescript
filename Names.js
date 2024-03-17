2c74484c1c43712b36cad067d00c3fcb2fe423dd
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-03-17 17:45:02 +0500

    Add files via upload

---
+++ Names.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question no:11;
+let personName = ['saba', 'umzehra', 'alisbah', 'sana'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Names.ts
@@ -0,0 +1,5 @@
+//Question no:11;
+let personName :string[] = ['saba','umzehra','alisbah','sana'];
+for(let i=0; i<names.length; i++){
+    console.log(names[i]);
+}
