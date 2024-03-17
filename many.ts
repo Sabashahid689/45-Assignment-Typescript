bc5099b3415f09ad545b575d058d1ec6322a9f1d
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-03-17 17:56:06 +0500

    Add files via upload

--- many.ts
+++ many.ts
@@ -1,1 +1,5 @@
-console.log("Hello, World");
+//BMI Calculator Class 05 HomeWork
+let mass = 70; //60kg
+let height = 17.5; // 16.5m
+let bmi = mass / (height*height)
+console.log(`Saba your BMI is ${bmi}`)
